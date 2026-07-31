d122995e67a3741638412c7ec2a0b0e2ef68de12
Jay Ta'ala <jay@jaytaala.com>
2026-07-31 23:17:52 +1000

    Remove the resetting of "overview_zoom_target" when closing zoom.  This allows niri to use the zoom target to as the originating zoom level in animating the closing of zoom/overview.
    It also allows for consistent behaviour when entering overview using
    the standard/normal "toggle-overview" keybind.  That is, it will enter
    overview at the zoom level last used.
    
    Finally, I also added updated insta snapshots.  All niri tests are now
    passing.

--- niri-config/src/lib.rs
+++ niri-config/src/lib.rs
@@ -1633,6 +1633,18 @@ mod tests {
                         ),
                     },
                 ),
+                overview_zoom: OverviewZoomAnim(
+                    Animation {
+                        off: false,
+                        kind: Spring(
+                            SpringParams {
+                                damping_ratio: 1.0,
+                                stiffness: 800,
+                                epsilon: 0.0001,
+                            },
+                        ),
+                    },
+                ),
                 recent_windows_close: RecentWindowsCloseAnim(
                     Animation {
                         off: true,
@@ -1699,6 +1711,7 @@ mod tests {
                         a: 0.3137255,
                     },
                 },
+                zoom_presets: None,
             },
             environment: Environment(
                 [
